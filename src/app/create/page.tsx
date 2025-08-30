94252a653d31000f3c7e4f8da26c68826e70ed96
NoahAldhous <naldhous@hotmail.co.uk>
2025-08-30 13:14:47 +0100

    create page now sends POST request to server, inserts new stronghold into db, returns stronghold id, redirects to hompage, which now fetches and shows list of strongholds

---
+++ src/app/create/page.tsx
@@ -0,0 +1,162 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useAuth } from "contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function Page() {
+  const router = useRouter();
+  const { isLoggedIn, logout, userId, userName } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [strongholdId, setStrongholdId] = useState(0);
+  const [displayModal, setDisplayModal] = useState(false);
+  const [strongholdTypes, setStrongholdTypes] = useState([
+    {
+      type_name: "",
+      id: 0,
+    },
+  ]);
+  const [userStronghold, setUserStronghold] = useState({
+    user_id: userId,
+    stronghold_name: "",
+    owner_name: "",
+    stronghold_level: 1,
+    stronghold_type: "",
+  });
+  const levels = [1,2,3,4,5]
+
+  //redirect user to login/signup page if not logged in
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    } else {
+      fetchStrongholdTypes();
+    }
+  }, [isLoggedIn]);
+
+  async function fetchStrongholdTypes() {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/strongholds/types`
+      );
+
+      if (!res.ok) {
+        throw new Error("Could not fetch data");
+      }
+
+      const data = await res.json();
+      console.log(data.data);
+      setStrongholdTypes(data.data);
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  async function handleSubmit() {
+    setLoading(true);
+
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/strongholds/create`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(userStronghold)
+        });
+
+        if (!res.ok) {
+            throw new Error("Error: Could not create stronghold")
+        }
+
+        //if success, will create message
+        const data = await res.json();
+        console.log(data)
+        //create message modal, with button which either - return to list of strongholds or navigates to created stronghold page
+        if (data.id) {
+            setStrongholdId(data.id)
+            setDisplayModal(true)
+        }
+
+    } catch (err) {
+        console.log(err.message)
+    } finally {
+        setLoading(false)
+    }
+  }
+
+  function handleStrongholdTypeChange(type: string) {
+    setUserStronghold({ ...userStronghold, stronghold_type: type });
+  };
+
+  function handleStrongholdLevelChange(level: number) {
+    setUserStronghold({ ...userStronghold, stronghold_level: level });
+  };
+
+  function handleOwnerNameChange(e: {target: {value: string}}) {
+    setUserStronghold({
+        ...userStronghold,
+        owner_name: e.target.value
+    });
+  };
+
+  function handleStrongholdNameChange(e: {target: {value: string}}) {
+    setUserStronghold({
+        ...userStronghold,
+        stronghold_name: e.target.value
+    });
+  };
+
+  return (
+    <>
+      <h1>Create a Stronghold</h1>
+        {strongholdTypes.map((item) => {
+          return <button onClick={() => handleStrongholdTypeChange(item.type_name)} key={item.id}>{item.type_name}</button>;
+        })}
+        <br/>
+        {levels.map((item) => {
+            return <button onClick={() => handleStrongholdLevelChange(item)} key={item}>{item}</button>
+        })}
+      <br/>
+      <label>
+        Stronghold Owner:
+        <input
+            type="text"
+            name="owner_name"
+            value={userStronghold.owner_name}
+            onChange={handleOwnerNameChange}
+            ></input>
+      </label>
+      <label>
+        Stronghold Name:
+        <input
+            type="text"
+            name="stronghold_name"
+            value={userStronghold.stronghold_name}
+            onChange={handleStrongholdNameChange}
+            ></input>
+      </label>
+      <h3>User Object</h3>
+      <p>
+        id: {userStronghold.user_id} <br />
+        name: {userStronghold.stronghold_name} <br />
+        owner_name: {userStronghold.owner_name} <br />
+        level: {userStronghold.stronghold_level} <br />
+        type: {userStronghold.stronghold_type} <br />
+      </p>
+      <br/>
+      <button onClick={handleSubmit}>CREATE</button>
+    {
+        displayModal ? 
+            <div>
+                <h4>Success! Your stronghold has been created</h4>
+                <Link href="/">Back to dashboard</Link>
+                <Link href={`/strongholds/${strongholdId}`}>View my stronghold id: {strongholdId}</Link>
+            </div>  
+        :
+            null
+    }
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,18 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "contexts/AuthContext";
 import LoginForm from "components/LoginForm/LoginForm";
 import SignUpForm from "components/SignUpForm/SignUpForm";
+import Link from "next/link";
 
 export default function Page() {
   const [loading, setLoading] = useState(false);
-  const [noStrongholds, setNoStrongholds] = useState(false);
+  const [noStrongholds, setNoStrongholds] = useState(true);
+  const [listOfStrongholds, setListOfStrongholds] = useState([{
+    id: 0,
+    stronghold_name: "",
+    owner_name:"",
+    stronghold_level:0,
+    stronghold_type_id:0,
+  },])
   const router = useRouter();
   const { isLoggedIn, logout, userId } = useAuth();
 
@@ -21,6 +29,8 @@ export default function Page() {
   }, [isLoggedIn]);
 
   async function fetchStrongholdsByUserId() {
+    setLoading(true)
+
     try {
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/strongholds/user/${userId}`);
@@ -29,11 +39,14 @@ export default function Page() {
         throw new Error("There was a problem getting a response.");
     }
 
+    //TODO: in api, change so object does not return 'data:', instead returns 'strongholds:'
     const data = await res.json();
     console.log(data.data)
     if (data.data.length === 0) {
         setNoStrongholds(true);
     } else {
+      setNoStrongholds(false)
+      setListOfStrongholds(data.data)
     }
     } catch (err) {
     console.log(err.message);
@@ -47,10 +60,21 @@ export default function Page() {
       <h1>This is the homepage!</h1>
       <h2>You should only be here if you are logged in!</h2>
       <button onClick={logout}>Log Out</button>
-      {noStrongholds ? (
-        <p>you have no strongholds! boo!</p>
+      {noStrongholds && !loading ? (
+        <div>
+          <p>you have no strongholds! boo!</p>
+          <Link href="/create">Create your first stronghold</Link>
+        </div>
       ) : (
-        <p>here are your strongholds:</p>
+        <div>
+          <p>here are your strongholds:</p>
+          <ul>
+            {listOfStrongholds.map((item, index) => {
+              return <li key={index}>name: {item.stronghold_name}</li>
+            })}
+          </ul>
+        </div>
+        
       )}
       {/* <LoginForm/>
         <SignUpForm/> */}
